26dc413f04c963accdc24c4cd3044b44d2761c60
guding0407 <hh5792@naver.com>
2025-11-26 17:36:20 +0900

    convert c to c++ v.2

--- Project_Tetris/testmain.cpp
+++ Project_Tetris/testmain.cpp
@@ -1,10 +1,11 @@
-#include "Tetris.h"
+#include "TetrisGame.h"
 
 int main() {
-    // 테트리스 객체 생성
-    Tetris game;
+    // 랜덤 시드 초기화
+    srand((unsigned)time(NULL));
 
-    // 게임 실행
+    // 게임 객체 생성 및 실행
+    TetrisGame game;
     game.run();
 
     return 0;
