8858a196b0465021f5b0d5ea15a9dad35ba41b5f
김도현 <dohyunkim04053@naver.com>
2025-12-02 17:38:42 +0900

    비트 마스킹 추가

--- Project_Tetris/TetrisBlock.h
+++ Project_Tetris/TetrisBlock.h
@@ -2,13 +2,16 @@
 
 class TetrisBlock {
 private:
-    // 7종류, 4회전, 4x4 크기의 블럭 데이터
-    static char shapes[7][4][4][4];
+    // [변경] 4x4x4 char 배열 -> 16비트 정수(unsigned short) 배열로 압축
+    // 예: 0010 0010 0010 0010 (2진수) -> 0x2222 (16진수)
+    static unsigned short shapes[7][4];
 
 public:
-    // 해당 shape, angle의 (r,c) 위치에 블럭이 있는지 반환 (1 or 0)
+    // (r, c) 위치에 블록이 존재하는지 비트 연산으로 확인
     static int getShape(int shape, int angle, int r, int c);
-    
-    // 블럭 종류별 색상 반환
+
+    // [추가] 성능 최적화를 위해 원본 비트마스크 값을 직접 가져오는 함수
+    static unsigned short getShapeMask(int shape, int angle);
+
     static int getColor(int shape);
 };
