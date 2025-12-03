59125f07c1dabeb36f94b41c24a3f0122a094ab4
guding0407 <hh5792@naver.com>
2025-12-03 15:49:38 +0900

    merge 충돌 해결

--- Project_Tetris/TetrisCore.cpp
+++ Project_Tetris/TetrisCore.cpp
@@ -1,4 +1,4 @@
-#include "TetrisCore.h"
+﻿#include "TetrisCore.h"
 #include "ConsoleHelper.h"
 
 TetrisCore::TetrisCore(int x, int y, bool waterMode, bool isBattle) {
@@ -7,17 +7,33 @@ TetrisCore::TetrisCore(int x, int y, bool waterMode, bool isBattle) {
     isWaterMode = waterMode;
     board.setBasePosition(ab_x, ab_y);
 
-    // 난이도 데이터 (기존 유지)
-    stage_data[0] = { 40, 20, 1, 300 };
-    stage_data[1] = { 38, 18, 1, 280 };
-    stage_data[2] = { 35, 18, 20, 250 };
-    stage_data[3] = { 30, 17, 20, 220 };
-    stage_data[4] = { 25, 16, 20, 200 };
-    stage_data[5] = { 20, 14, 20, 180 };
-    stage_data[6] = { 15, 14, 20, 160 };
-    stage_data[7] = { 10, 13, 20, 140 };
-    stage_data[8] = { 6, 12, 20, 120 };
-    stage_data[9] = { 4, 11, 99999, 80 };
+    // 난이도 데이터 초기화
+    if (isBattle) {
+        // 2인용 (빠른 레벨업)
+        stage_data[0] = { 40, 20, 5, 300 };
+        stage_data[1] = { 38, 18, 5, 280 };
+        stage_data[2] = { 35, 18, 5, 250 };
+        stage_data[3] = { 30, 17, 5, 220 };
+        stage_data[4] = { 25, 16, 5, 200 };
+        stage_data[5] = { 20, 14, 5, 180 };
+        stage_data[6] = { 15, 14, 5, 160 };
+        stage_data[7] = { 10, 13, 5, 140 };
+        stage_data[8] = { 6, 12, 5, 120 };
+        stage_data[9] = { 4, 11, 99999, 80 };
+    }
+    else {
+        // 1인용 (기본)
+        stage_data[0] = { 40, 20, 20, 300 };
+        stage_data[1] = { 38, 18, 20, 280 };
+        stage_data[2] = { 35, 18, 20, 250 };
+        stage_data[3] = { 30, 17, 20, 220 };
+        stage_data[4] = { 25, 16, 20, 200 };
+        stage_data[5] = { 20, 14, 20, 180 };
+        stage_data[6] = { 15, 14, 20, 160 };
+        stage_data[7] = { 10, 13, 20, 140 };
+        stage_data[8] = { 6, 12, 20, 120 };
+        stage_data[9] = { 4, 11, 99999, 80 };
+    }
 }
 
 void TetrisCore::initGame(int startLevel) {
@@ -56,6 +72,7 @@ void TetrisCore::draw() {
 void TetrisCore::updateLogic() {
     if (is_gameover) return;
 
+    // 해수면 모드 로직
     if (isWaterMode) {
         water_tick_count++;
         if (water_tick_count >= stage_data[level].water_speed) {
@@ -70,6 +87,7 @@ void TetrisCore::updateLogic() {
         }
     }
 
+    // 블록 자동 낙하 로직
     speed_counter++;
     if (speed_counter >= stage_data[level].speed) {
         speed_counter = 0;
@@ -171,7 +189,6 @@ void TetrisCore::checkFullLine() {
     }
 }
 
-// [수정] showGameStat: printf -> write
 void TetrisCore::showGameStat() {
     int uiX = ab_x + 30;
     int uiY = ab_y + 2;
@@ -188,39 +205,34 @@ void TetrisCore::showGameStat() {
     ConsoleHelper::writeInt(uiX + 6, uiY + 9, remainLines, WHITE);
 }
 
-// [수정] showNextBlock: printf -> write
 void TetrisCore::showNextBlock(int shape) {
     int uiX = ab_x + 30;
     int uiY = ab_y + 1;
 
-    // 지우기
+    // 지우기 (표준 공백 8칸)
     for (int i = 0; i < 4; i++) ConsoleHelper::write(uiX, uiY + i, "        ", BLACK);
 
-    // 그리기
+    // 그리기 (네모 문자)
     for (int r = 0; r < 4; r++) {
         for (int c = 0; c < 4; c++) {
             if (TetrisBlock::getShape(shape, 0, r, c)) {
-                // [중요] write 사용
                 ConsoleHelper::write((c * 2) + uiX, r + uiY, "■", TetrisBlock::getColor(shape));
             }
         }
     }
 }
 
-// [수정] showCurBlock: printf -> write (잔상 해결 핵심)
 void TetrisCore::showCurBlock(int shape, int angle, int x, int y) {
     for (int r = 0; r < 4; r++) {
         for (int c = 0; c < 4; c++) {
             if (!TetrisBlock::getShape(shape, angle, r, c)) continue;
             if (y + r < 0) continue;
 
-            // [중요] setCursorPosition + printf 대신 write 사용
             ConsoleHelper::write((c + x) * 2 + ab_x, y + r + ab_y, "■", TetrisBlock::getColor(shape));
         }
     }
 }
 
-// [수정] eraseCurBlock: printf -> write
 void TetrisCore::eraseCurBlock(int shape, int angle, int x, int y) {
     for (int r = 0; r < 4; r++) {
         for (int c = 0; c < 4; c++) {
@@ -234,7 +246,7 @@ void TetrisCore::eraseCurBlock(int shape, int angle, int x, int y) {
                 ConsoleHelper::write((c + x) * 2 + ab_x, gy + ab_y, "■", WATER_COLOR);
             }
             else {
-                // [중요] write 사용
+                // 표준 공백 2칸
                 ConsoleHelper::write((c + x) * 2 + ab_x, gy + ab_y, "  ", BLACK);
             }
         }
@@ -242,40 +254,19 @@ void TetrisCore::eraseCurBlock(int shape, int angle, int x, int y) {
 }
 
 int TetrisCore::strikeCheck(int shape, int angle, int x, int y) {
-    // [최적화] 함수 호출 반복 대신 비트마스크를 한 번 가져옴
-    unsigned short mask = TetrisBlock::getShapeMask(shape, angle);
-    unsigned short bitChecker = 0x8000; // 1000 0000 0000 0000 (최상위 비트)
-
-    for (int i = 0; i < 16; i++) {
-        // 해당 비트가 1인 경우에만 충돌 검사 수행 (비트가 0이면 루프 패스)
-        if (mask & bitChecker) {
-            int r = i / 4; // 행
-            int c = i % 4; // 열
-
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
+            if (!TetrisBlock::getShape(shape, angle, r, c)) continue;
             int gy = y + r;
             int gx = x + c;
 
-
-            if (gx <= 0 || gx >= 13) return 1;
+            if (gx <= 0 || gx >= 13) return 1; // 벽
             if (gy < 0) continue;
-            if (gy >= 21) return 1;
+            if (gy >= 21) return 1; // 바닥
 
-            int blockType = board.getBlock(gy, gx);
-            if (blockType != EMPTY_BLOCK) return 1;
-=======
-            // 1. 벽 충돌 (좌우)
-            if (gx <= 0 || gx >= 13) return 1;
-
-            // 2. 바닥 충돌
-            if (gy >= 21) return 1;
-
-            // 3. 보드판의 다른 블록과 충돌 (화면 위쪽(gy < 0)은 검사 제외)
-            if (gy >= 0) {
-                if (board.getBlock(gy, gx) != EMPTY_BLOCK) return 1;
-            }
+            // 보드판 블록 충돌
+            if (board.getBlock(gy, gx) != EMPTY_BLOCK) return 1;
         }
-        // 검사 비트를 오른쪽으로 한 칸 이동
-        bitChecker >>= 1;
     }
     return 0;
 }
@@ -298,7 +289,7 @@ int TetrisCore::moveBlock(int* shape, int* angle, int* x, int* y, int* next_shap
 
     if (strikeCheck(*shape, *angle, *x, *y)) {
         (*y)--;
-        if (*y < 0) return 1;
+        if (*y < 0) return 1; // 게임 오버
 
         mergeBlock(*shape, *angle, *x, *y);
         *shape = *next_shape;
